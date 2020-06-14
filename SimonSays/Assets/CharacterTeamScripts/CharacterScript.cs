fc8f9b58bce92dcecf51be9b80fe7751b01940e0
samipya <snirmal@usc.edu>
2020-06-13 20:16:41 -0700

    Before Demo 13th June

---
+++ SimonSays/Assets/CharacterTeamScripts/CharacterScript.cs
@@ -0,0 +1,112 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.VersionControl;
+using UnityEngine;
+
+
+public class CharacterScript : MonoBehaviour
+{
+   
+    float speed = 5.0f;
+    float height = 5.0f;
+    Rigidbody rigidBody;
+    bool left;
+    bool right;
+    int WrongLettersCollected;
+    private Vector3 m_Move;
+    private Animator m_Animator;
+    private float m_currentV = 0;
+    private float m_currentH = 0;
+    private readonly float m_interpolation = 10; 
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+        m_Animator = GetComponent<Animator>();
+        WrongLettersCollected = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(0, 0, speed * Time.deltaTime);
+
+        Vector3 newRight = new Vector3( 8.2f, transform.position.y, transform.position.z);
+        Vector3 newLeft = new Vector3(-8.2f, transform.position.y,transform.position.z);
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+            bool TWOfinger = Input.touches.Length > 1;
+            if (!TWOfinger)
+            {
+                if (touch.position.magnitude > Screen.width / 2)
+                {
+                    right = true;
+                    left = false;
+                }
+                if (touch.position.magnitude < Screen.width / 2)
+                {
+                    right = false;
+                    left = true;
+                }
+            }
+            else
+            {
+                rigidBody.velocity = Vector3.zero;
+                rigidBody.velocity = Vector3.up * height;
+            }
+            if (left == true)
+            {
+                transform.position = Vector3.LerpUnclamped(transform.position, newLeft, speed*Time.deltaTime);
+            }
+            if (right == true)
+            {
+                transform.position = Vector3.LerpUnclamped(transform.position, newRight, speed*Time.deltaTime);
+            }
+
+        }
+
+    }
+    void FixedUpdate()
+    {
+
+        Transform camera = Camera.main.transform;
+        Vector3 direction = camera.forward * m_currentV + camera.right * m_currentH;
+        
+        m_currentV = Mathf.Lerp(m_currentV, 1, Time.deltaTime * m_interpolation);
+        m_currentH = Mathf.Lerp(m_currentH, 1, Time.deltaTime * m_interpolation);
+
+        float directionLength = direction.magnitude;
+        direction.y = 0;
+        direction = direction.normalized * directionLength;
+
+        m_Animator.SetFloat("MoveSpeed", direction.magnitude);
+       
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Correct")
+        {
+            Destroy(other.gameObject);
+            Debug.Log("Correct letter collected");
+        }
+
+        if(other.gameObject.tag=="Incorrect")
+        {
+            Destroy(other.gameObject);
+            WrongLettersCollected++;
+            Debug.Log("Wrong letter collected");
+        }
+        if(other.gameObject.tag=="Obstacle")
+        {
+            //Rotation code
+            Destroy(other.gameObject);
+            Debug.Log("Obstacle collected");
+        }
+    }
+
+}
---
+++ UpdatedScriptDemo/CharacterScript.cs
@@ -0,0 +1,140 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.VersionControl;
+using UnityEngine;
+using UnityStandardAssets.Characters.ThirdPerson;
+
+public class CharacterScript : MonoBehaviour
+{
+   
+    public float speed = 5.0f;
+    public float height = 5.0f;
+    public float plainLeftMax;
+    public float plainRightMax;
+    public float leftRightSpeed;
+    Rigidbody rigidBody;
+    bool left;
+    bool right;  
+    private Animator m_Animator;
+    private float m_currentV = 0;
+    private float m_currentH = 0;
+    private readonly float m_interpolation = 10;
+ 
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+        m_Animator = GetComponent<Animator>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(0, 0, speed * Time.deltaTime);
+        Vector3 newRight = new Vector3( plainRightMax, transform.position.y, transform.position.z);
+        Vector3 newLeft = new Vector3(plainLeftMax, transform.position.y,transform.position.z);   
+        if(Input.anyKey)
+        {
+            if ( Input.GetKeyDown(KeyCode.RightArrow))
+            {
+                right = true;
+                left = false;
+            }
+            if ( Input.GetKeyDown(KeyCode.LeftArrow))
+            {
+                right = false;
+                left = true;
+            }
+            if(Input.GetKeyDown(KeyCode.UpArrow))
+            {
+                rigidBody.velocity = Vector3.zero;
+                rigidBody.velocity = Vector3.up * height;
+            }
+            if (left == true)
+            {
+                transform.position = Vector3.LerpUnclamped(transform.position, newLeft, 3*leftRightSpeed * Time.deltaTime);
+                left = false;
+            }
+            if (right == true)
+            {
+                transform.position = Vector3.LerpUnclamped(transform.position, newRight, 3*leftRightSpeed * Time.deltaTime);
+                right = false;
+            }
+        }
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+            bool TWOfinger = Input.touches.Length > 1;
+            if (!TWOfinger)
+            {
+
+                if (touch.position.magnitude > Screen.width / 2 )
+                {
+                    right = true;
+                    left = false;
+                }
+                if (touch.position.magnitude < Screen.width / 2)
+                {
+                    right = false;
+                    left = true;
+                }
+            }
+            if(TWOfinger)
+            {
+                rigidBody.velocity = Vector3.zero;
+                rigidBody.velocity = Vector3.up * height;
+            }
+            if (left == true)
+            {
+                transform.position = Vector3.LerpUnclamped(transform.position, newLeft, leftRightSpeed * Time.deltaTime);
+                left = false;
+            }
+            if (right == true)
+            {
+                transform.position = Vector3.LerpUnclamped(transform.position, newRight, leftRightSpeed * Time.deltaTime);
+                right = false;
+            }
+
+        }
+
+    }
+    void FixedUpdate()
+    {
+
+        Transform camera = Camera.main.transform;
+        Vector3 direction = camera.forward * m_currentV + camera.right * m_currentH;
+        
+        m_currentV = Mathf.Lerp(m_currentV, 1, Time.deltaTime * m_interpolation);
+        m_currentH = Mathf.Lerp(m_currentH, 1, Time.deltaTime * m_interpolation);
+
+        float directionLength = direction.magnitude;
+        direction.y = 0;
+        direction = direction.normalized * directionLength;
+
+        m_Animator.SetFloat("MoveSpeed", direction.magnitude);
+       
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Correct")
+        {
+            Destroy(other.gameObject);
+            Debug.Log("Correct letter collected");
+        }
+
+        if(other.gameObject.tag=="Incorrect")
+        {
+            Destroy(other.gameObject);
+            Debug.Log("Wrong letter collected");
+        }
+        if(other.gameObject.tag=="Obstacle")
+        {
+            //Rotation code
+            Destroy(other.gameObject);
+            Debug.Log("Obstacle collected");
+        }
+    }
+
+}
