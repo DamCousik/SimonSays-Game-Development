83d65495ddd38148015f7180ebdd9cb97c41b93b
rankaashi <66390167+rankaashi@users.noreply.github.com>
2020-06-13 17:58:57 -0700

    Add files via upload
    Added All Scenese

---
+++ SimonSays/Assets/ZoneFourClick.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+ using System.Collections;
+ using UnityEngine.EventSystems;
+ using UnityEngine.SceneManagement;
+ 
+ public class ZoneFourClick : MonoBehaviour {
+
+     private void Update(){
+
+         if (Input.GetMouseButtonDown(0) && !EventSystem.current.IsPointerOverGameObject()){
+             Debug.Log("Doing Ray test");
+             RaycastHit hit;
+             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+             if (Physics.Raycast(ray, out hit, Mathf.Infinity))
+             {
+                 //Debug.Log("Selected" +hit.name);
+                 Debug.Log("Hit Somthing" +hit.transform.tag);
+                 //Replace this with whatever logic you want to use to validate the objects you want to click on
+                 if(hit.collider.gameObject.name == "zone4")
+                 {
+                     SceneManager.LoadScene("Zone4Scene");
+                 }
+             }
+         }
+
+
+     }
+ }
